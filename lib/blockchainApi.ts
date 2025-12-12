8a15d9ff72fb07e7e8ba14a15fbaaa6219ad77a2
Daniil Kozyr <dkozyr@mtb.minsk.by>
2025-12-12 18:21:47 +0300

    Testing two parallel repos

--- lib/blockchainApi.ts
+++ lib/blockchainApi.ts
@@ -289,8 +289,6 @@ type EthplorerTx = {
   value?: number;    // значение уже в ETH
 };
 
-// lol
-
 async function fetchEthereumAddressTransactions(
   address: string,
 ): Promise<BlockchainTx[]> {
